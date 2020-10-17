10cd2c7996d3c261dd13850b183bb4dc0e3f4ffa
yeasinmahi <yeasinmahi72@yahoo.com>
2020-10-17 17:17:23 +0600

    Improbve some code

--- app/src/main/java/com/devfolder/moradnagarshop/MainActivity.kt
+++ app/src/main/java/com/devfolder/moradnagarshop/MainActivity.kt
@@ -2,35 +2,35 @@ package com.devfolder.moradnagarshop
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.*
 
 class MainActivity : AppCompatActivity() {
-    private var tgwebView: WebView? = null
-    var myAdView: AdView? = null
-    var mInterstitialAd: InterstitialAd? = null
+    private var webView: WebView? = null
+    private var myAdView: AdView? = null
     private var interstitial: InterstitialAd? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        CheckInternet()
-        MobileAds.initialize(this, getString(R.string.app_id))
-        BnrAdd()
+        this.checkInternet()
+        MobileAds.initialize(this)
+        loadBannerAd()
     }
 
-    fun BnrAdd() {
+    private fun loadBannerAd() {
         myAdView = findViewById<View>(R.id.adView) as AdView
         val adRequest = AdRequest.Builder().build()
         myAdView!!.loadAd(adRequest)
     }
 
-    fun prepareAd() {
+    fun loadInterstialAd() {
         // Prepare the Interstitial Ad
         interstitial = InterstitialAd(this@MainActivity)
         // Insert the Ad Unit ID
@@ -47,7 +47,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun displayInterstitial() {
-        // If Ads are loaded, show Interstitial else show nothing.
         if (interstitial!!.isLoaded) {
             interstitial!!.show()
         }
@@ -70,8 +69,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if (tgwebView!!.canGoBack()) {
-            tgwebView!!.goBack()
+        if (webView!!.canGoBack()) {
+            webView!!.goBack()
         } else {
             super.onBackPressed()
         }
@@ -80,23 +79,32 @@ class MainActivity : AppCompatActivity() {
     val isConnected: Boolean
         get() {
             val cm = this.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
-            if (null != cm) {
-                val info = cm.activeNetworkInfo
-                return info != null && info.isConnected
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                var n = cm.activeNetwork
+                if (n != null) {
+                    var nc = cm.getNetworkCapabilities(n)
+                    return (nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || nc.hasTransport(
+                        NetworkCapabilities.TRANSPORT_WIFI
+                    ));
+                }
+            } else {
+                var ni = cm.getActiveNetworkInfo();
+                if (ni != null) {
+                    return (ni.isConnected() && (ni.getType() == ConnectivityManager.TYPE_WIFI || ni.getType() == ConnectivityManager.TYPE_MOBILE));
+                }
             }
             return false
         }
 
-    private fun CheckInternet() {
+
+    private fun checkInternet() {
         val mWebClient: WebViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
                 if (isConnected) {
                     view.loadUrl(url)
-                    if (url.contains("travelettesofbangladesh.com/blog") || url.contains("travelettesofbangladesh.com/videos") || url.contains(
-                            "travelettesofbangladesh.com/event"
-                        )
-                    ) {
-                        prepareAd()
+                    if (url.contains(getString(R.string.interstitial_url_1)) || url.contains(getString(
+                                                R.string.interstitial_url_2))) {
+                        loadInterstialAd()
                     }
                 } else {
                     buildDialog(this@MainActivity).show()
@@ -107,16 +115,14 @@ class MainActivity : AppCompatActivity() {
             override fun onLoadResource(view: WebView, url: String) {}
         }
         //start checking internet connection
-        if (!isConnected(this)) {
+        if (!isConnected) {
             buildDialog(this@MainActivity).show()
         } else {
-            //Toast.makeText(this@MainActivity, "Welcome Travelettes!!", Toast.LENGTH_SHORT).show()
-            // setContentView(R.layout.activity_main);
-            tgwebView = findViewById<View>(R.id.webView) as WebView
-            tgwebView!!.webViewClient = mWebClient
-            val webSettings = tgwebView!!.settings
+            webView = findViewById<View>(R.id.webView) as WebView
+            webView!!.webViewClient = mWebClient
+            val webSettings = webView!!.settings
             webSettings.javaScriptEnabled = true
-            tgwebView!!.loadUrl("https://muradnagarshop.com")
+            webView!!.loadUrl(getString(R.string.url))
         }
         //end checking internet connection
     }
@@ -125,27 +131,16 @@ class MainActivity : AppCompatActivity() {
         val builder = AlertDialog.Builder(
             c!!
         )
-        builder.setTitle("No Internet Connection")
-        builder.setMessage("You need to have Mobile Data or WiFi to access this app.")
+        builder.setTitle(getString(R.string.no_internet_title))
+        builder.setMessage(getString(R.string.no_internet_message))
         builder.setPositiveButton(
             "Retry"
-        ) { dialog, which -> CheckInternet() }
-        builder.setOnDismissListener { CheckInternet() }
-        return builder
-    } //end internet connection dialogue
-
-    companion object {
-        //start internet connection dialogue
-        fun isConnected(context: Context): Boolean {
-            val cm = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
-            val netinfo = cm.activeNetworkInfo
-            return if (netinfo != null && netinfo.isConnectedOrConnecting) {
-                val wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
-                val mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
-                mobile != null && mobile.isConnectedOrConnecting || wifi != null && wifi.isConnectedOrConnecting
-            } else {
-                false
-            }
+        ) { _, _ -> checkInternet() }
+        builder.setOnDismissListener {
+            checkInternet()
         }
+        return builder
     }
+
+
 }
